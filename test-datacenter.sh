2139758a529ee91e0379a2154cedcd7a67b49c5b
StarCloud <liu.xiao.hu.net@gmail.com>
2016-10-22 03:57:38 +0800

    for macos and openwrt

--- test-datacenter.sh
+++ test-datacenter.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-bold=$(tput bold)
-normal=$(tput sgr0)
+if [ "$(which tput)" != "" ];then
+	bold=$(tput bold)
+	normal=$(tput sgr0)
+else
+	bold=""
+	normal=""
+fi
 
 better_ping=99999
 better_datacenter=''
@@ -10,12 +15,16 @@ max_ping=$1
 
 while read -r line;
 do
-	echo -n "Ping ${bold}${line}${normal}: "
-	ping_avg=`ping -c $max_ping $line | tail -n1 | cut -d'=' -f2 | cut -d'/' -f2`
-	echo "$ping_avg ms"
-	if [ $(echo "$ping_avg < $better_ping" | bc) -eq 1 ];
-	then
-		better_ping=$ping_avg
+	if [ "$(uname)" == "Darwin" ];then
+		ping_avg=`ping -W 2 -c $max_ping $line | tail -n1 | cut -d'=' -f2 | cut -d'/' -f2`
+		echo "Ping ${bold}${line}${normal}: $ping_avg ms"
+	elif [ "$(uname)" == "Linux" ];then
+		echo -n "Ping ${bold}${line}${normal}: "
+		ping_avg=`ping -W 2 -c $max_ping $line | tail -n1 | cut -d'=' -f2 | cut -d'/' -f2`
+		echo "$ping_avg ms"
+	fi
+	if [ $(echo "$ping_avg"|cut -d'.' -f1) -lt $better_ping ];then
+		better_ping=$(echo "$ping_avg"|cut -d'.' -f1)
 		better_datacenter=$line
 		better_datacenter_name=`echo $line | cut -d'.' -f1 | cut -d'-' -f2`
 	fi
